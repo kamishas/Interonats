9c2b2c6c3b35c1c20c91c26b4e87f28d75a44421
kamishas <kaminenishasank@gmail.com>
2026-02-05 16:07:50 -0500

    Remove all root-level junk files from Git

--- check_status.py
+++
@@ -1,8 +0,0 @@
-import boto3
-def check():
-    client = boto3.client('lambda', region_name='us-east-2')
-    resp = client.get_function(FunctionName='onehr-login-api')
-    print(resp['Configuration']['LastUpdateStatus'])
-
-if __name__ == '__main__':
-    check()
